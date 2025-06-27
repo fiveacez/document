c729dadd2a3b80cebdaad9e73379f19555527c82
RonaldodeLema <vanthao000009@gmail.com>
2025-06-27 11:19:03 +0700

    fix: update

--- my-docs/docusaurus.config.ts
+++ my-docs/docusaurus.config.ts
@@ -15,10 +15,11 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://fiveacez.github.io',
+  // url: 'https://fiveacez.github.io',
+  url: 'https://ai.document.vietnam2030.vn',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ai-vietnam2030',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
